95de9c51bdedbad346a87dd1a73afbb7e0ebe2ba
Bhavya Sharma <mc.coding006@gmail.com>
2026-06-08 22:46:35 +0530

    Time: 0 ms (100%), Space: 20.9 MB (29.32%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -20,7 +20,7 @@ public:
         // marked all the extra array
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(row[i] == 1 or col[j] == 1){
+                if(row[i] == 1 || col[j] == 1){
                     matrix[i][j] = 0;
                 }
                 else{
