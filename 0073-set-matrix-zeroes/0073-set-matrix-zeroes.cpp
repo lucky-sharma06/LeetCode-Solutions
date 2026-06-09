02b0d2723ab68342d28f420b7715f7644cf5e12e
Bhavya Sharma <mc.coding006@gmail.com>
2026-06-09 11:48:10 +0530

    Time: 0 ms (100%), Space: 20.8 MB (49.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,31 +3,51 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        // m x n mattrix, m rows and n columns
-
-        vector<int> row(m, 0);
-        vector<int> col(n, 0);
-
+        //  m is row and n is col
+        // col -> matrix[0][...]
+        // row -> matrix[...][0]
+        int col0 = matrix[0][0];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(matrix[i][j] == 0){
-                    row[i] = 1;
-                    col[j] = 1;
+                    matrix[i][0] = 0;
+                    if(j == 0){
+                        col0 = 0;
+                    }
+                    else{
+                        matrix[0][j] = 0;
+                    }
                 }
             }
         }
 
-        // marked all the extra array
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(row[i] == 1 || col[j] == 1){
+        // marking is complete...
+        // now traverse without the 0th row and 0th col
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
-                }
-                else{
-                    continue;
                 }
             }
         }
+        // till now except 0 row and 0 col everything is done
+        // since col is dependent on row, so we will do col first
+
+        // col is done
+        for(int j = 1; j < n; j++){
+            if(matrix[0][0] == 0){
+                matrix[0][j] = 0;
+            }
+        }
+
+        // now about row
+        for(int i = 0; i < m; i++){
+            if(col0 == 0){
+                matrix[i][0] = 0;
+            }
+        }
+
+    
         return;
     }
 };
