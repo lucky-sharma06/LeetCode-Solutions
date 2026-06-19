50e7d878bcda2287ddb3974ee4e83237261feddd
Bhavya Sharma <mc.coding006@gmail.com>
2026-06-19 21:02:14 +0530

    Time: 0 ms (100%), Space: 12.6 MB (71.43%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        // Brute Force
+        // Optimal
         int n = nums.size();
-        if(n == 1) return 0;
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n - 1] > nums[n - 2]) return n - 1;
+        int low = 1; int high = n - 2;
+        if(n == 1) return 0; // single elemnt
+        if(nums[0] > nums[1]) return 0; // first elem
+        if(nums[n - 1] > nums[n - 2]) return n - 1; // last elem
 
-        for(int i = 1; i <= n - 1; i++){
-            if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
-                return i;
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+
+            if(nums[mid] > nums[mid - 1]){
+                // increasing curve
+                low = mid + 1;
+            }
+            else if(nums[mid] > nums[mid + 1]){
+                // decreasing curve
+                high = mid - 1;
+            }
+            else{
+                // it is on the dip
+                low = mid + 1;
+            }
         }
         return -1;
     }
