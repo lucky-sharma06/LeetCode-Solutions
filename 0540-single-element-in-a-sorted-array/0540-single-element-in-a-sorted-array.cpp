c6a8c58a81bfd06f41ce59280067b53bdf158c55
Bhavya Sharma <mc.coding006@gmail.com>
2026-06-19 20:09:16 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -17,7 +17,7 @@ public:
             if(((mid % 2 == 1) && (nums[mid] == nums[mid - 1])) || ((mid % 2 == 0) && (nums[mid] == nums[mid + 1]))){
                 low = mid + 1;
             }
-            // (odd, even) right
+            // (odd, even) right half
             else{
                 high = mid - 1;
             }
