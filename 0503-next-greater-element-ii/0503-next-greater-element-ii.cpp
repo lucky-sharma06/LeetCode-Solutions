a5f62610b7fa1b8104f4aac70dab86496db20999
Bhavya Sharma <mc.coding006@gmail.com>
2026-07-16 21:21:01 +0530

    Time: 121 ms (5.27%), Space: 28.6 MB (39.66%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> ans;
+        // double the array approach
+        for(int i = 0; i < nums.size(); i++){
+            bool found = false;
+            for(int j = i + 1; j <= i + nums.size() - 1; j++){
+                int idx = j % nums.size();
+                if(nums[idx] > nums[i]){
+                    ans.push_back(nums[idx]);
+                    found = true;
+                    break;
+                }
+            }
+            if(found == false){
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
