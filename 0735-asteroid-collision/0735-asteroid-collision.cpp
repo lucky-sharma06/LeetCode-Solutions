e1921a38210c3dfc9a3b90bdf79856cfe3cfb1f0
Bhavya Sharma <mc.coding006@gmail.com>
2026-07-18 16:17:28 +0530

    Time: 4 ms (14.78%), Space: 23.2 MB (6.46%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        list<int> l;
+        int n = asteroids.size();
+        for(int i = 0; i < n; i++){
+            if(asteroids[i] > 0){
+                // +ve asteroids come
+                l.push_back(asteroids[i]);
+            }
+            else{
+                // -ve aaya 
+                // while(l.empty() || l.back() < 0){
+                //     l.push_back(asteroids[i]);
+                // }
+
+                while(!l.empty() && l.back() > 0 && l.back() < abs(asteroids[i])){
+                    // smaller getting destroyed by negatives
+                    l.pop_back();
+                }
+                
+                if(!l.empty() && l.back() == abs(asteroids[i])){
+                    l.pop_back(); // if equal hai then both destryoed so move ahead
+                    continue;
+                }
+                if(l.empty() || l.back() < 0){
+                    l.push_back(asteroids[i]); // -ve to be pushed in the list
+                }
+            }
+        }
+        vector<int> ans;
+        for(auto val : l){
+            ans.push_back(val);
+        }
+        return ans;
+    
+    }
+};
