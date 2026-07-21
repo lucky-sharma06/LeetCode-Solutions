47e4e4c0d404ad8ff137f6c170a6795fc872f676
Bhavya Sharma <mc.coding006@gmail.com>
2026-07-21 22:27:03 +0530

    Time: 5 ms (75.23%), Space: 20.8 MB (41.87%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int larRecHist(vector<int> &arr){
+        int maxArea = 0;
+        int n = arr.size();
+        stack<int> st;
+        int nse = 0; 
+        int pse = 0;
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                // we want < elements in the stack 
+                int elem = st.top();
+                st.pop();
+                nse = i;
+                pse = st.empty() ? -1 : st.top();
+                maxArea = max(maxArea, arr[elem] * (nse - pse - 1));
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            int elem = st.top();
+            st.pop();
+            nse = n;
+            pse = st.empty() ? -1 : st.top();
+            maxArea = max(maxArea, arr[elem] * (nse - pse - 1));
+        }
+
+        return maxArea;        
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> prefixSum(m, vector<int>(n));
+
+        for(int j = 0; j < n; j++){
+            int sum = 0;
+            for(int i = 0; i < m; i++){
+                sum += matrix[i][j] - '0';
+                if(matrix[i][j] - '0' == 0){
+                    sum = 0;
+                }
+                prefixSum[i][j] = sum;
+            }
+        }
+        // prefixSum matrix is now ready
+        // now we will continue passing the mat[i] to the function
+        int maxArea = 0;
+        for(int i = 0; i < m; i++){
+            maxArea = max(maxArea, larRecHist(prefixSum[i]));
+        }
+        return maxArea;
+    }
+};
