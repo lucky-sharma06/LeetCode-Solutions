887390e333c3e7766e0a20a94f17bd539fb765f5
Bhavya Sharma <mc.coding006@gmail.com>
2026-08-07 00:25:07 +0530

    Time: 0 ms (100%), Space: 52.1 MB (98.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int jumpNeeded = 1;
-        for(int i = n - 2; i >= 0; i--){
-            if(nums[i] < jumpNeeded){
-                // means i can't make the jumb from here
-                jumpNeeded++;
-            }
-            else{
-                jumpNeeded = 1;
-            }
+        int maxIndex = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxIndex) return false;
+            maxIndex = max(maxIndex, nums[i] + i);
         }
-        if(jumpNeeded == 1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 };
