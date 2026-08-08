891fafc5b213e47cee6b2dfa2decbbe98497495f
Bhavya Sharma <mc.coding006@gmail.com>
2026-08-08 23:02:34 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
+    double helper(double x, long long m){
+        if(m == 0) return 1;
+
+        if(m & 1 == 1){
+            // odd
+            return x * helper(x * x, m / 2);
+        }
+        return helper(x * x, m / 2);
+    }
     double myPow(double x, int n) {
-        double ans = 1;
         long long m = n;
-        m = abs(m);
-        while(m > 0){
-            if(m & 1 == 1){
-                // odd
-                ans = ans * x;
-                m--;
-            }
-            else{
-                // even
-                m = m / 2;
-                x = x * x;
-            }
+        if(m < 0){
+            return 1.0/helper(x, -m);
         }
-        if(n < 0){
-            ans = 1.0/ans;
-        }
-        return ans;
+        return helper(x, m);
     }
 };
