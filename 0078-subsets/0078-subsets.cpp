2ffe79cc047734cd6b578a2ffcc671d3a0a2b7b5
Bhavya Sharma <mc.coding006@gmail.com>
2026-08-13 22:18:50 +0530

    Time: 0 ms (100%), Space: 9.7 MB (96.61%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> powerSet;
-        int n = nums.size();
-        int totalSubset = (1 << n);
-
-        for(int i = 0; i < totalSubset; i++){
-            vector<int> subset;
-            for(int j = 0; j < n; j++){
-                if((i & (1 << j)) != 0){
-                    // means the jth bit is set
-                    subset.push_back(nums[j]);
-                }
-            }
-            powerSet.push_back(subset);
+    void subSet(int idx, vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds){
+        if(idx == nums.size()){
+            ans.push_back(ds);
+            return;
         }
-        return powerSet;
+
+        ds.push_back(nums[idx]);
+        subSet(idx + 1, nums, ans, ds); // pick
+        ds.pop_back();
+        subSet(idx + 1, nums, ans, ds); // non-pick
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        subSet(0, nums, ans, ds);
+
+        return ans;
     }
 };
