601110d8d6ad33ef8580d00cfd95b678e82a5598
Bhavya Sharma <mc.coding006@gmail.com>
2026-08-14 17:38:06 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.9%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void findCombo(int idx, int k, int n, vector<vector<int>>& ans, vector<int>& ds){
-        if(n == 0 && k == 0){ // base case
-            ans.push_back(ds); // we are getting the sum
+        if(k == 0){
+            if(n == 0){ // base case
+                ans.push_back(ds); // we are getting the sum
+            }
             return;
         }
 
         for(int i = idx + 1; i < 10; i++){
             if(i > n) break;
-            k--;
             ds.push_back(i);
-            findCombo(i, k, n - i, ans, ds);
+            findCombo(i, k - 1, n - i, ans, ds);
             ds.pop_back();
-            k++;
         }
 
     }
