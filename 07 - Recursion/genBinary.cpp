695bf15c48282735cfd7dde9a6df15b541f816a4
Bhavya Sharma <mc.coding006@gmail.com>
2026-08-23 00:08:16 +0530

    Add files via upload

--- 07 - Recursion/genBinary.cpp
+++ 07 - Recursion/genBinary.cpp
@@ -10,9 +10,12 @@ void genBinary(int idx, int n, vector<string>& ans, string& ds){
     ds.push_back('0');
     genBinary(idx + 1, n, ans, ds);
     ds.pop_back();
-    ds.push_back('1');
-    genBinary(idx + 1, n, ans, ds);
-    ds.pop_back();
+    if(ds.empty() || ds.back() != '1'){
+        
+        ds.push_back('1');
+        genBinary(idx + 1, n, ans, ds);
+        ds.pop_back();
+    }
 
 }
 int main(){
