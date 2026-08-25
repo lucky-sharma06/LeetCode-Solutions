434d4338dca805dddd53c76635c0003f3aff4e85
Bhavya Sharma <mc.coding006@gmail.com>
2026-08-25 22:06:06 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.26%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,34 +1,7 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, int n, vector<string>& board){
-        // check for the left upper diagonal
-        int r = row; 
-        int c = col;
-        while(row >= 0 && col >= 0){
-            if(board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-
-        row = r;
-        col = c;
-        // check for the left columns
-        while(col >= 0){
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-
-        row = r;
-        col = c;
-        // check for the left lower diagonal
-        while(col >= 0 && row < n){
-            if(board[row][col] == 'Q') return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-    void Queen(int col, vector<string>& board, vector<vector<string>>& ans){
+    // This solution is without using isSafe function...
+    void Queen(int col, int n, vector<string>& board, vector<vector<string>>& ans, vector<int>& left, vector<int>& upperDiagonal, vector<int>& lowerDiagonal){
         // Base case: 
         if(col == board.size()){
             ans.push_back(board);
@@ -36,11 +9,17 @@ public:
         }
 
         for(int row = 0; row < board.size(); row++){
-            if(isSafe(row, col, board.size(), board)){
-                // it is safe to put the queen
+            if(left[row] == 0 && upperDiagonal[(n-1) + (col - row)] == 0 && lowerDiagonal[row + col] == 0){
+                // it is now safe to enter the queen
                 board[row][col] = 'Q';
-                Queen(col + 1, board, ans);
+                left[row] = 1;
+                upperDiagonal[(n-1) + (col - row)] = 1;
+                lowerDiagonal[row + col] = 1;
+                Queen(col + 1, n, board, ans, left, upperDiagonal, lowerDiagonal);
                 board[row][col] = '.';
+                left[row] = 0;
+                upperDiagonal[(n-1) + (col - row)] = 0;
+                lowerDiagonal[row + col] = 0;
             }
         }
     }
@@ -53,7 +32,11 @@ public:
             board[i] = s;
         }
         // created the chess board with '.'
-        Queen(0, board, ans);
+
+        // three hash arrays
+        vector<int> left(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0); 
+
+        Queen(0, board.size(), board, ans, left, upperDiagonal, lowerDiagonal);
 
         return ans;
 
